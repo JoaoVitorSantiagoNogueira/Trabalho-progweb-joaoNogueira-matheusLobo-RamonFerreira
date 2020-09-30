9583cf733655eccae89d2f3af4b0acd0c82d6f92
Matheus Lobo <matheuslobo@id.uff.br>
2020-09-30 18:14:02 -0300

    Modo escuro adicionado na tela de login

---
+++ javascript/darkmode.js
@@ -0,0 +1,9 @@
+function ToggleDarkMode() {
+    document.body.classList.toggle("dark-mode")
+
+    if (document.body.classList.length > 0) {
+        document.getElementById("darkmodeIcon").src = "img/sun.png"
+    }else{
+        document.getElementById("darkmodeIcon").src = "img/moon.png"
+    }
+}
