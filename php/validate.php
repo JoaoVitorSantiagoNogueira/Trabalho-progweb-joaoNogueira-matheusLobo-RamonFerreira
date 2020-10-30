172820b6d83c90a05736fd89f0b9ff8e64800aad
JoaoVitorSantiagoNogueira <65747315+JoaoVitorSantiagoNogueira@users.noreply.github.com>
2020-10-29 21:22:33 -0300

    Implementação basica da comincação
    Implementação basica do php realizando a comunicação com o sql. Ela só funciona para a tela de login e ela não é segura uma vez que o usuario poderia requisitar por buscas diferentes. Idealmente uma busca interna seria feita dentro db que retornaria somente verdadeiro ou falso  para a presença uma linha. Atualmente somente o bd agora tem uma pessoa a com senha b para testes.

---
+++ php/validate.php
@@ -0,0 +1,23 @@
+<?php
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	//database conection
+	$conn = mysqli_connect('localhost','root','','gerenciadordb');
+	if($conn->connect_error){
+		die('Connection Failed : ' .$conn->connect_error);
+	}else{
+		$query= mysqli_query($conn , "SELECT * FROM usuario WHERE nomeUsuario = '".$username."' AND senha = '".$password."'");	
+		if (!$query){
+			die('Error: ' . mysqli_error($conn));
+    			}
+		if($query->num_rows == 0){
+		   //place holder
+  	 	   echo "account not found";   
+		}else{
+		   //place holder
+   		   echo "logging in";   
+		}
+	}
+	$conn->close();
+	$query->close();
+?>
---
+++ sql/gerenciadordb.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2020 at 01:17 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gerenciadordb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url`
+--
+
+CREATE TABLE `url` (
+  `id` int(11) NOT NULL,
+  `url` text NOT NULL,
+  `usuarioID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nomeUsuario` varchar(20) NOT NULL,
+  `senha` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='tabela basica de usuario';
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nomeUsuario`, `senha`) VALUES
+(1, 'a', 'b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `url`
+--
+ALTER TABLE `url`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuarioID` (`usuarioID`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `url`
+--
+ALTER TABLE `url`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `url`
+--
+ALTER TABLE `url`
+  ADD CONSTRAINT `url_ibfk_1` FOREIGN KEY (`usuarioID`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
